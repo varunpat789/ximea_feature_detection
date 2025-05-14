8cf18c8a5a12c2d8caafb7059244d0b36802378e
varunpat789 <varunpat789@gmail.com>
2025-05-13 23:32:40 -0400

    initial file strucuture

---
+++ ximea_odometry/include/ximea_odometry/ximea_feature_detection.hpp
@@ -0,0 +1,17 @@
+#ifndef XIMEA_ODOMETRY__XIMEA_FEAT_DETECTION_NODE_HPP_
+#define XIMEA_ODOMETRY__XIMEA_FEAT_DETECTION_NODE_HPP_
+
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/camera_info.hpp"
+#include "sensor_msgs/msg/image.hpp"
+#include "std_msgs/msg/int16.hpp"
+
+class CameraNode : public rclcpp::Node {
+ public:
+ 
+ private:
+
+};
+
+
+#endif  // XIMEA_ODOMETRY__XIMEA_FEAT_DETECTION_NODE_HPP_
---
+++ ximea_odometry/src/ximea_feature_detection.cpp
@@ -0,0 +1,11 @@
+#include "ximea_odometry/ximea_feature_detection.hpp"
+
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
